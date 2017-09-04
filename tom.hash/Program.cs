4707d9a11e4a4e992cd3082d0453cd574225ec93
Tom Postler <tom@postler.me>
2017-09-03 21:50:22 -0500

    Start hash

---
+++ tom.hash/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Unlimitedinf.Tom.Hash
+{
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    public sealed class Program : ITom
+    {
+        /// <inheritdoc/>
+        public string Name => "hash";
+
+        /// <inheritdoc/>
+        public string Description => "Wraps the hashing components of Unlimitedinf.Tools for ease of use.";
+
+        /// <inheritdoc/>
+        public bool IsAsync => true;
+
+        /// <inheritdoc/>
+        public void Run(string[] args) => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public Task RunAsync(string[] args) => throw new NotImplementedException();
+    }
+}
