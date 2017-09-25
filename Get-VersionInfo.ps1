ceaa383f5f7e36cdd13715e256b48fa6d017c1fd
Tom Postler <tom@postler.me>
2017-09-25 15:31:20 -0500

    Offline version mode

--- Get-VersionInfo.ps1
+++ Get-VersionInfo.ps1
@@ -5,10 +5,41 @@ param(
     [Parameter(Mandatory=$true)][string]$AssemblyName
 )
 
+function GetResponseGobble {
+    # In the case where we get an exception with a specific message,
+    #   we're probably offline and should just gobble the exception instead.
+    param (
+        [Parameter(Mandatory=$true)][System.Net.WebRequest]$Request,
+        [Parameter(Mandatory=$true)][String]$Default
+    )
+
+    try
+    {
+        $response = $Request.GetResponse();
+        # Check for failure
+	    if (!($response.StatusCode -eq [System.Net.HttpStatusCode]::OK)) {
+		    Write-Host "Request failed with $($response.StatusCode)";
+		    exit 1;
+	    }
+        return (New-Object System.IO.StreamReader $response.GetResponseStream()).ReadToEnd();
+    }
+    catch [System.Net.WebException]
+    {
+        # If the message does not contain the following, then we have a different problem.
+        if (-not $_.Exception.Message.StartsWith("The remote name could not be resolved: '")) {
+            throw;
+        }
+        Write-Host -ForegroundColor Yellow "Could not talk to internet. Using default values..."
+        return $Default;
+    }
+}
+
 $urlbase = "https://unlimitedinf-apis.azurewebsites.net";
 $VersionName = "tom.exe_$AssemblyName";
 $CountName = "tom.exe_$($AssemblyName)";
 $count = "0";
+$DefaultCount = [math]::Floor([timespan]::new([datetime]::Now.Ticks - [datetime]::Now.Date.Ticks).TotalMinutes);
+$DefaultVersion = "{0}.{1}.{2}" -f 0, [datetime]::Now.Year.ToString().Substring(2), [datetime]::Now.DayOfYear;
 
 # If we have the token defined, try to update the count. Swallow all errors.
 if ($env:UnlimitedinfApiToken -ne $null) {
@@ -21,18 +52,12 @@ if ($env:UnlimitedinfApiToken -ne $null) {
 	$reqStream = New-Object System.IO.StreamWriter $request.GetRequestStream();
 	$reqStream.Write(('{{"username":"unlimitedinf","name":"{0}","type":"inc"}}' -f $CountName));
 	$reqStream.Close();
-	$response = $request.GetResponse();
+	$result = GetResponseGobble -Request $request -Default ('{{"count":{0}}}' -f $DefaultCount);
 
-	# Check for failure
-	if (!($response.StatusCode -eq [System.Net.HttpStatusCode]::OK)) {
-		Write-Host "Request to increase count failed with $($response.StatusCode)";
-	}
-	else {
-		# Get the count
-		$count = (New-Object System.IO.StreamReader $response.GetResponseStream()).ReadToEnd() | ConvertFrom-Json;
-		$count = $count.count.ToString();
-		Write-Host "Count $CountName was updated to $count";
-	}
+	# Get the count
+	$count = $result | ConvertFrom-Json;
+	$count = $count.count.ToString();
+	Write-Host "Count $CountName was updated to $count";
 }
 # Else just get the count
 else {
@@ -40,16 +65,10 @@ else {
 	Write-Host "Retrieving current count information from $($url.SubString($urlbase.Length))";
 	$request = [System.Net.WebRequest]::CreateHttp($url);
 	$request.Method = "GET";
-	$response = $request.GetResponse();
-
-	# Check for failure
-	if (!($response.StatusCode -eq [System.Net.HttpStatusCode]::OK)) {
-		Write-Host "Request for count failed with $($response.StatusCode)";
-		exit 1;
-	}
+	$result = GetResponseGobble -Request $request -Default ('{{"count":{0}}}' -f $DefaultCount);
 
 	# Get the version
-	$count = (New-Object System.IO.StreamReader $response.GetResponseStream()).ReadToEnd() | ConvertFrom-Json;
+	$count = $result | ConvertFrom-Json;
 	$count = $count.count.ToString();
 	Write-Host "Count $CountName was getted as $count";
 }
@@ -58,16 +77,10 @@ $url = "$urlbase/versioning/versions?username=unlimitedinf&versionName=$VersionN
 Write-Host "Retrieving current version information from $($url.SubString($urlbase.Length))";
 $request = [System.Net.WebRequest]::CreateHttp($url);
 $request.Method = "GET";
-$response = $request.GetResponse();
-
-# Check for failure
-if (!($response.StatusCode -eq [System.Net.HttpStatusCode]::OK)) {
-    Write-Host "Request for version failed with $($response.StatusCode)";
-    exit 1;
-}
+$result = GetResponseGobble -Request $request -Default ('{{"version":"{0}"}}' -f $DefaultVersion);
 
 # Get the version
-$version = (New-Object System.IO.StreamReader $response.GetResponseStream()).ReadToEnd() | ConvertFrom-Json;
+$version = $result | ConvertFrom-Json;
 $version = $version.version;
 Write-Host "Version $VersionName was getted as $version";
 
