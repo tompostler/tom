9214bdd7d5edd9d03613c7510c726e492fe75c5c
Tom Postler <tom@postler.me>
2022-11-26 11:26:41 -0600

    optional indented json

--- src/util/Extensions/ObjectExtensions.cs
+++ src/util/Extensions/ObjectExtensions.cs
@@ -8,17 +8,22 @@ namespace Unlimitedinf.Utilities.Extensions
     /// </summary>
     public static class ObjectExtensions
     {
+        private static readonly JsonSerializerOptions indentedOptions;
         private static readonly JsonSerializerOptions options;
+
         static ObjectExtensions()
         {
+            indentedOptions = new JsonSerializerOptions();
+            indentedOptions.Converters.Add(new JsonStringEnumConverter());
+            indentedOptions.WriteIndented = true;
+
             options = new JsonSerializerOptions();
             options.Converters.Add(new JsonStringEnumConverter());
-            options.WriteIndented = true;
         }
 
         /// <summary>
         /// Convert an object to a string of indented json.
         /// </summary>
-        public static string ToJsonString(this object value) => JsonSerializer.Serialize(value, options);
+        public static string ToJsonString(this object value, bool indented = false) => JsonSerializer.Serialize(value, indented ? indentedOptions : options);
     }
 }
