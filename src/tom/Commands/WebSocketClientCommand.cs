09bb916f3c67b3e6c9acf758c6d64a6cd2f15e1e
Tom Postler <tom@postler.me>
2022-11-26 12:22:48 -0600

    and subject-name cert validation

--- src/tom/Commands/WebSocketClientCommand.cs
+++ src/tom/Commands/WebSocketClientCommand.cs
@@ -3,10 +3,12 @@ using System;
 using System.CommandLine;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Security;
 using System.Net.WebSockets;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Unlimitedinf.Utilities;
@@ -30,9 +32,14 @@ namespace Unlimitedinf.Tom.Commands
                 "The working directory to be used for serving files. Only files that are a children of this directory can be copied.");
             command.AddOption(workingDirOpt);
 
+            Option<string> sslCertSubjectNameOpt = new(
+                "--ssl-cert-subject",
+                "Used in scenarios where the HTTPS certificate is a valid and trusted SSL certificate but the remote name used to reach the destination won't match the presented certificate.");
+            command.AddOption(sslCertSubjectNameOpt);
+
             Option<string> sslCertThumbprintOpt = new(
                 "--ssl-cert-thumbprint",
-                "Used in scenarios where the HTTPS certificate is not expected to match the hostname of the endpoint.");
+                "Used in scenarios where the HTTPS certificate is a self-signed certificate and the only validation possible is by thumbprint.");
             command.AddOption(sslCertThumbprintOpt);
 
             Option<TimeSpan> maxSessionDurationOpt = new(
@@ -41,11 +48,11 @@ namespace Unlimitedinf.Tom.Commands
                 "After this amount of time, the session will be aborted with the server.");
             command.AddOption(maxSessionDurationOpt);
 
-            command.SetHandler(HandleAsync, endpointArg, workingDirOpt, sslCertThumbprintOpt, maxSessionDurationOpt);
+            command.SetHandler(HandleAsync, endpointArg, workingDirOpt, sslCertSubjectNameOpt, sslCertThumbprintOpt, maxSessionDurationOpt);
             return command;
         }
 
-        private static async Task HandleAsync(Uri endpoint, DirectoryInfo workingDir, string sslCertThumbprint, TimeSpan maxSessionDuration)
+        private static async Task HandleAsync(Uri endpoint, DirectoryInfo workingDir, string sslCertSubjectName, string sslCertThumbprint, TimeSpan maxSessionDuration)
         {
             Environment.CurrentDirectory = workingDir.FullName;
             using CancellationTokenSource cts = new(maxSessionDuration);
@@ -54,7 +61,7 @@ namespace Unlimitedinf.Tom.Commands
 
             // For non-endpoint matching or self-signed certificates, validate them manually
             // (self-signed certificates are perfectly valid for TLS as long as you are validating the certificate is the one you expect)
-            bool serverCertificateValidationCallback(X509Certificate certificate, SslPolicyErrors sslPolicyErrors)
+            bool serverCertificateValidationCallback(X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
             {
                 // If there are no errors, then the endpoint is completely valid (regardless of us providing a specific thumbprint)
                 if (sslPolicyErrors == SslPolicyErrors.None)
@@ -62,27 +69,68 @@ namespace Unlimitedinf.Tom.Commands
                     return true;
                 }
 
+                // If a subject name is provided, then perfom that validation
+                if (!string.IsNullOrWhiteSpace(sslCertSubjectName))
+                {
+                    var errorMessage = new StringBuilder($"Remote certificate error: {sslPolicyErrors}.");
+
+                    // If there are chain errors, then it's an invalid certificate (expired, self-signed, etc)
+                    if (sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateChainErrors))
+                    {
+                        _ = errorMessage.AppendLine($"Chain errors: {string.Join(";", chain?.ChainStatus.Select(c => $"{c.Status}:{c.StatusInformation}"))}");
+                        Console.Error.WriteLine(errorMessage.ToString());
+                        return false;
+                    }
+
+                    // Reject on any other SslPolicyErrors we're not explicitly handling
+                    // SslPolicyErrors is a flag enum and ~ is a bitwise inversion. So this basically reads as "if there are any bits set in sslPolicyErrors that are not RemoteCertificateNameMismatch"
+                    if ((sslPolicyErrors & ~SslPolicyErrors.RemoteCertificateNameMismatch) != 0)
+                    {
+                        _ = errorMessage.AppendLine($"Unhandled {nameof(SslPolicyErrors)} ({sslPolicyErrors})");
+                        Console.Error.WriteLine(errorMessage.ToString());
+                        return false;
+                    }
+
+                    // This is the only other location where we may process a valid request
+                    if (sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateNameMismatch))
+                    {
+                        const string cnPrefix = "CN=";
+                        string cnComponent = certificate.Subject.Split(',').Single(c => c.StartsWith(cnPrefix)).Substring(cnPrefix.Length).Trim();
+
+                        if (string.Equals(sslCertSubjectName, cnComponent, StringComparison.OrdinalIgnoreCase))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            _ = errorMessage.AppendLine($"Subject name [{certificate.Subject}] (parsed: {cnComponent}) was not expected: {sslCertSubjectName}");
+                            Console.Error.WriteLine(errorMessage.ToString());
+                            return false;
+                        }
+                    }
+                }
+
                 // Only return true if the thumprint of the certificate matches what we're expecting
-                return string.Equals(sslCertThumbprint, certificate?.GetCertHashString());
+                return string.Equals(sslCertThumbprint, certificate?.GetCertHashString(), StringComparison.OrdinalIgnoreCase);
             }
 
             // Set up the basic httpClient
             HttpClient httpClient = new();
-            if (!string.IsNullOrWhiteSpace(sslCertThumbprint))
+            if (!string.IsNullOrWhiteSpace(sslCertSubjectName) || !string.IsNullOrWhiteSpace(sslCertThumbprint))
             {
                 HttpClientHandler handler = new()
                 {
-                    ServerCertificateCustomValidationCallback = (_, certificate, _, sslPolicyErrors) => serverCertificateValidationCallback(certificate, sslPolicyErrors)
+                    ServerCertificateCustomValidationCallback = (_, certificate, chain, sslPolicyErrors) => serverCertificateValidationCallback(certificate, chain, sslPolicyErrors)
                 };
                 httpClient = new(handler);
             }
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
-            
+
             // Set up the web socket client
             ClientWebSocket client = new();
-            if (!string.IsNullOrWhiteSpace(sslCertThumbprint))
+            if (!string.IsNullOrWhiteSpace(sslCertSubjectName) || !string.IsNullOrWhiteSpace(sslCertThumbprint))
             {
-                client.Options.RemoteCertificateValidationCallback = (_, certificate, _, sslPolicyErrors) => serverCertificateValidationCallback(certificate, sslPolicyErrors);
+                client.Options.RemoteCertificateValidationCallback = (_, certificate, chain, sslPolicyErrors) => serverCertificateValidationCallback(certificate, chain, sslPolicyErrors);
             }
             client.Options.SetRequestHeader(HeaderNames.Authorization, httpClient.DefaultRequestHeaders.Authorization.ToString());
 
