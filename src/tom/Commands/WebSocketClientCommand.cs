fcf0eaa754cedc86d7b7991fb6823adf5211e1d3
Tom Postler <tom@postler.me>
2022-11-26 12:04:24 -0600

    successful pinging

--- src/tom/Commands/WebSocketClientCommand.cs
+++ src/tom/Commands/WebSocketClientCommand.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Net.Http.Headers;
+using System;
 using System.CommandLine;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Net.Security;
 using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
+using Unlimitedinf.Utilities;
 
 namespace Unlimitedinf.Tom.Commands
 {
@@ -25,10 +30,10 @@ namespace Unlimitedinf.Tom.Commands
                 "The working directory to be used for serving files. Only files that are a children of this directory can be copied.");
             command.AddOption(workingDirOpt);
 
-            Option<string> httpsCertThumbprintOpt = new(
-                "--https-cert-thumbprint",
+            Option<string> sslCertThumbprintOpt = new(
+                "--ssl-cert-thumbprint",
                 "Used in scenarios where the HTTPS certificate is not expected to match the hostname of the endpoint.");
-            command.AddOption(httpsCertThumbprintOpt);
+            command.AddOption(sslCertThumbprintOpt);
 
             Option<TimeSpan> maxSessionDurationOpt = new(
                 "--max-session-duration",
@@ -36,37 +41,64 @@ namespace Unlimitedinf.Tom.Commands
                 "After this amount of time, the session will be aborted with the server.");
             command.AddOption(maxSessionDurationOpt);
 
-            command.SetHandler(HandleAsync, endpointArg, workingDirOpt, httpsCertThumbprintOpt, maxSessionDurationOpt);
+            command.SetHandler(HandleAsync, endpointArg, workingDirOpt, sslCertThumbprintOpt, maxSessionDurationOpt);
             return command;
         }
 
-        private static async Task HandleAsync(Uri endpoint, DirectoryInfo workingDir, string httpsCertThumbprint, TimeSpan maxSessionDuration)
+        private static async Task HandleAsync(Uri endpoint, DirectoryInfo workingDir, string sslCertThumbprint, TimeSpan maxSessionDuration)
         {
             Environment.CurrentDirectory = workingDir.FullName;
             using CancellationTokenSource cts = new(maxSessionDuration);
-
-
-
-            ClientWebSocket client = new();
+            string token = Input.GetString("Authorization token");
+            Console.WriteLine();
 
             // For non-endpoint matching or self-signed certificates, validate them manually
             // (self-signed certificates are perfectly valid for TLS as long as you are validating the certificate is the one you expect)
-            if (!string.IsNullOrWhiteSpace(httpsCertThumbprint))
+            bool serverCertificateValidationCallback(X509Certificate certificate, SslPolicyErrors sslPolicyErrors)
             {
-                client.Options.RemoteCertificateValidationCallback = (_, certificate, _, sslPolicyErrors) =>
+                // If there are no errors, then the endpoint is completely valid (regardless of us providing a specific thumbprint)
+                if (sslPolicyErrors == SslPolicyErrors.None)
                 {
-                    // If there are no errors, then the endpoint is completely valid (regardless of us providing a specific thumbprint)
-                    if (sslPolicyErrors == SslPolicyErrors.None)
-                    {
-                        return true;
-                    }
+                    return true;
+                }
 
-                    // Only return true if the thumprint of the certificate matches what we're expecting
-                    return string.Equals(httpsCertThumbprint, certificate?.GetCertHashString());
-                };
+                // Only return true if the thumprint of the certificate matches what we're expecting
+                return string.Equals(sslCertThumbprint, certificate?.GetCertHashString());
             }
 
-            await client.ConnectAsync(endpoint, cts.Token);
+            // Set up the basic httpClient
+            HttpClient httpClient = new();
+            if (!string.IsNullOrWhiteSpace(sslCertThumbprint))
+            {
+                HttpClientHandler handler = new()
+                {
+                    ServerCertificateCustomValidationCallback = (_, certificate, _, sslPolicyErrors) => serverCertificateValidationCallback(certificate, sslPolicyErrors)
+                };
+                httpClient = new(handler);
+            }
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+            
+            // Set up the web socket client
+            ClientWebSocket client = new();
+            if (!string.IsNullOrWhiteSpace(sslCertThumbprint))
+            {
+                client.Options.RemoteCertificateValidationCallback = (_, certificate, _, sslPolicyErrors) => serverCertificateValidationCallback(certificate, sslPolicyErrors);
+            }
+            client.Options.SetRequestHeader(HeaderNames.Authorization, httpClient.DefaultRequestHeaders.Authorization.ToString());
+
+            // First attempt a basic ping to make sure the routing, authentication, and ssl validation are all correct
+            Uri pingEndpoint = endpoint.Scheme.StartsWith("ws") ? new($"http{endpoint.AbsoluteUri[2..]}") : endpoint;
+            Console.WriteLine($"Verifying route, auth, and ssl by hitting ping endpoint at {pingEndpoint}...");
+            var sw = Stopwatch.StartNew();
+            HttpResponseMessage pingResponse = await httpClient.GetAsync(new Uri(pingEndpoint, "ws/ping"), cts.Token);
+            if (pingResponse.StatusCode != System.Net.HttpStatusCode.NoContent)
+            {
+                throw new InvalidOperationException($"Could not successfully ping wss. Status code {pingResponse.StatusCode} with body {await pingResponse.Content.ReadAsStringAsync(cts.Token)}");
+            }
+            Console.WriteLine($"Successful ping in {sw.ElapsedMilliseconds}ms.");
+            Console.WriteLine();
+
+            //await client.ConnectAsync(new Uri(endpoint, "ws/connect"), cts.Token);
         }
     }
 }
