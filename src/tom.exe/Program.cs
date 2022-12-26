6376fc84880beb352c0aac5969bb12aa074f0f75
Tom Postler <tom@postler.me>
2022-12-25 18:11:51 -0600

    add console color tester

--- src/tom.exe/Program.cs
+++ src/tom.exe/Program.cs
@@ -18,6 +18,7 @@ namespace Unlimitedinf.Tom
             rootCommand.AddCommand(Commands.RandomCommand.Create());
             rootCommand.AddCommand(Commands.WebSocketClientCommand.Create());
 
+            rootCommand.AddCommand(Commands.ZzTestConsoleColorsCommand.Create());
             rootCommand.AddCommand(Commands.ZzTestConsoleProgressLoggerCommand.Create());
 
             return await rootCommand.InvokeAsync(args);
---
+++ src/tom/Commands/ZzTestConsoleColorsCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CommandLine;
+
+namespace Unlimitedinf.Tom.Commands
+{
+    internal sealed class ZzTestConsoleColorsCommand
+    {
+        public static Command Create()
+        {
+            Command command = new("zz-test-console-colors", "Test the output of the ConsoleColor enum to see what the terminal supports.");
+
+            Option<ConsoleColor> backgroundColorOpt = new(
+                "--background-color",
+                () => Console.BackgroundColor,
+                "The background color to display against.");
+            command.AddOption(backgroundColorOpt);
+
+            command.SetHandler(Handle, backgroundColorOpt);
+            return command;
+        }
+
+        public static void Handle(ConsoleColor backgroundColor)
+        {
+            ConsoleColor currentForeground = Console.ForegroundColor;
+            ConsoleColor currentBackground = Console.BackgroundColor;
+
+            Console.BackgroundColor = backgroundColor;
+            foreach (ConsoleColor foregroundColor in Enum.GetValues<ConsoleColor>())
+            {
+                Console.ForegroundColor = foregroundColor;
+                Console.WriteLine($"Foreground: {foregroundColor}, Background: {backgroundColor}");
+            }
+            
+            Console.ForegroundColor = currentForeground;
+            Console.BackgroundColor = currentBackground;
+
+            Console.WriteLine();
+        }
+    }
+}
