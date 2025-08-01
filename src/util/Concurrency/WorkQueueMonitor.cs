b6775a4c569afd127e38bf34364a958efa44e786
Tom Postler <tom@postler.me>
2025-08-01 16:12:51 -0500

    separate monitoring with and without estimates

--- src/util/Concurrency/WorkQueueMonitor.cs
+++ src/util/Concurrency/WorkQueueMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Unlimitedinf.Utilities.Concurrency
 {
@@ -16,6 +17,7 @@ namespace Unlimitedinf.Utilities.Concurrency
     public sealed class WorkQueueMonitor(ILogger logger)
     {
         private readonly ILogger logger = logger;
+        private readonly object estimatedTimeLoggerLock = new();
 
         /// <summary>
         /// An optimization on <see cref="MonitorAsync{T}(Task[], ConcurrentQueue{T}, double, CancellationToken, int)"/> so you don't have to set up workers yourself.
@@ -26,12 +28,14 @@ namespace Unlimitedinf.Utilities.Concurrency
         /// <param name="workerCount">Number of worker tasks to spin up in parallel. Generally assumed to be less than 100.</param>
         /// <param name="cancellationToken"></param>
         /// <param name="reportingIntervalSeconds">How often to report the progress.</param>
+        /// <param name="includeEstimateToCompletion">If true, will add an additional line with the time remaining estimate.</param>
         public async Task WorkAndMonitorAsync<T>(
             ConcurrentQueue<T> workItems,
             Func<T, CancellationToken, Task<string>> workItemHandler,
             int workerCount,
             CancellationToken cancellationToken,
-            int reportingIntervalSeconds = 7)
+            int reportingIntervalSeconds = 7,
+            bool includeEstimateToCompletion = true)
         {
             int workItemOriginalCount = workItems.Count;
 
@@ -39,7 +43,11 @@ namespace Unlimitedinf.Utilities.Concurrency
             {
                 while (workItems.TryDequeue(out T workItem))
                 {
-                    this.logger.LogInformation($"[{id:D2}] {await workItemHandler(workItem, cancellationToken)}");
+                    string workResult = await workItemHandler(workItem, cancellationToken);
+                    lock (this.estimatedTimeLoggerLock)
+                    {
+                        this.logger.LogInformation($"[{id:D2}] {workResult}");
+                    }
                 }
             }
 
@@ -49,18 +57,58 @@ namespace Unlimitedinf.Utilities.Concurrency
                 workers[i] = worker(i);
             }
 
-            await this.MonitorAsync(
-                workers,
-                workItems,
-                workItemOriginalCount,
-                cancellationToken,
-                reportingIntervalSeconds);
+            if (includeEstimateToCompletion)
+            {
+                await this.MonitorWithEstimatedCompletionAsync(
+                    workers,
+                    workItems,
+                    workItemOriginalCount,
+                    cancellationToken,
+                    reportingIntervalSeconds);
+            }
+            else
+            {
+                await this.MonitorAsync(
+                    workers,
+                    workItems,
+                    workItemOriginalCount,
+                    cancellationToken,
+                    reportingIntervalSeconds);
+            }
+        }
+
+        /// <summary>
+        /// Given an array of worker tasks, a queue of work items, and the original count of work items, monitor the progress of the workers.
+        /// </summary>
+        public async Task MonitorAsync<T>(
+            Task[] workers,
+            ConcurrentQueue<T> workItems,
+            double workItemOriginalCount,
+            CancellationToken cancellationToken,
+            int reportingIntervalSeconds = 7)
+        {
+            var duration = Stopwatch.StartNew();
+            var sleepTime = TimeSpan.FromSeconds(reportingIntervalSeconds);
+            while (!workItems.IsEmpty)
+            {
+                await Task.Delay(sleepTime, cancellationToken);
+
+                this.logger.LogInformation(
+                    "---> {itemCompleteCount,5} ({itemCompletePercent,6:p}) work items complete ({itemRemainingCount,5} ({itemRemainingPercent,6:p}) work items remaining). {timeElapsed:hh\\:mm\\:ss} elapsed.",
+                    workItemOriginalCount - workItems.Count,
+                    (workItemOriginalCount - workItems.Count) / workItemOriginalCount,
+                    workItems.Count,
+                    workItems.Count / workItemOriginalCount,
+                    duration.Elapsed);
+            }
+
+            await this.MonitorToFinishAsync(workers, sleepTime, cancellationToken);
         }
 
         /// <summary>
         /// Given an array of worker tasks, a queue of work items, and the original count of work items, monitor the progress of the workers and the queue with time estimates based on recently completed work.
         /// </summary>
-        public async Task MonitorAsync<T>(
+        public async Task MonitorWithEstimatedCompletionAsync<T>(
             Task[] workers,
             ConcurrentQueue<T> workItems,
             double workItemOriginalCount,
@@ -93,36 +141,53 @@ namespace Unlimitedinf.Utilities.Concurrency
                     double avgSlice = diffs.Average();
                     double perSec = Math.Max(Math.Abs(avgSlice) / sleepTime.TotalSeconds, 0.00001);
 
-                    this.logger.LogInformation(
-                        "---> {itemCompleteCount,5} ({itemCompletePercent,6:p}) work items complete ({itemRemainingCount,5} ({itemRemainingPercent,6:p}) work items remaining). " +
-                        "Estimated {timeRemaining:hh\\:mm\\:ss} remaining ({timeElapsed:hh\\:mm\\:ss} elapsed, {rateCompletion,5:0.00} items/sec for last {historyCheckpointCount} checkpoints).",
+                    lock (this.estimatedTimeLoggerLock)
+                    {
+                        this.logger.LogInformation(
+                        "---> {itemCompleteCount,5} ({itemCompletePercent,6:p}) work items complete ({itemRemainingCount,5} ({itemRemainingPercent,6:p}) work items remaining).",
                         workItemOriginalCount - workItems.Count,
                         (workItemOriginalCount - workItems.Count) / workItemOriginalCount,
                         workItems.Count,
-                        workItems.Count / workItemOriginalCount,
+                        workItems.Count / workItemOriginalCount);
+                        this.logger.LogInformation(
+                        "---> Estimated {timeRemaining:hh\\:mm\\:ss} remaining ({timeElapsed:hh\\:mm\\:ss} elapsed, {rateCompletion,5:0.00} items/sec for last {historyCheckpointCount} checkpoints).",
                         TimeSpan.FromSeconds(workItems.Count / perSec),
                         duration.Elapsed,
                         perSec,
                         historyBuffer.Count);
+                    }
                 }
                 else
                 {
                     this.logger.LogInformation(
-                        "---> {workRemainingCount,5} ({workRemainingPercent,6:p}) work items remaining ({pollsToHistory} polls until there is enough history for estimation).",
+                        "---> {itemCompleteCount,5} ({itemCompletePercent,6:p}) work items complete ({itemRemainingCount,5} ({itemRemainingPercent,6:p}) work items remaining). {timeElapsed:hh\\:mm\\:ss} elapsed, and {pollsToHistory} polls until there is enough history for estimation.",
+                        workItemOriginalCount - workItems.Count,
+                        (workItemOriginalCount - workItems.Count) / workItemOriginalCount,
                         workItems.Count,
                         workItems.Count / workItemOriginalCount,
+                        duration.Elapsed,
                         threshold - historyBuffer.Count + 1);
                 }
             }
-            this.logger.LogInformation("Scanning all workers....");
+
+            await this.MonitorToFinishAsync(workers, sleepTime, cancellationToken);
+        }
+
+        private async Task MonitorToFinishAsync(
+            Task[] workers,
+            TimeSpan sleepTime,
+            CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("---> Scanning all workers....");
             while (workers.Any(t => !t.IsCompleted))
             {
                 this.logger.LogInformation("---> {workerCount} workers still working....", workers.Count(t => !t.IsCompleted));
                 await Task.Delay(sleepTime, cancellationToken);
             }
-            this.logger.LogInformation("Awaiting all workers....");
+
+            this.logger.LogInformation("---> Awaiting all workers....");
             await Task.WhenAll(workers);
-            this.logger.LogInformation("Workers are done working.");
+            this.logger.LogInformation("---> Workers are done working.");
         }
     }
 }
